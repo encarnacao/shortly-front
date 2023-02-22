63128d95db9065aa5c86b4243382246721c35b15
encarnacao <cencarnacaoq@gmail.com>
2023-02-22 18:37:20 -0300

    feat: create authorization context

---
+++ src/contexts/authContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+	const [user, setUser] = useState(null);
+	const [token, setToken] = useState(null);
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	return (
+		<AuthContext.Provider value={{ user, config, setUser, setToken }}>
+			{children}
+		</AuthContext.Provider>
+	);
+}
