a71a43dcc59ec4afb814ba64b15ca121d80f4eb0
encarnacao <cencarnacaoq@gmail.com>
2023-02-25 16:10:20 -0300

    refactor: change folders for clarity

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GlobalStyle from "./globalstyle";
+import GlobalStyle from "./styles/globalstyle";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./contexts/authContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
