e1dd4a334125ba1a2dd8e1b3425e65b3c45d9b45
encarnacao <cencarnacaoq@gmail.com>
2023-02-25 16:01:03 -0300

    feat: add navigate to several routes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,45 @@
 import { Title, Logo, HeaderContainer, Menu, SubHeader } from "./HeaderStyles";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/authContext";
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/shortly.svg";
 
-
 function setClass(item, active) {
-    let className = item === active ? "active" : "";
-    className += item === "Sair" ? " logout" : "";
-    return className;
+	let className = item === active ? "active" : "";
+	className += item === "Sair" ? " logout" : "";
+	return className;
 }
 
 function Header() {
 	const { active, setActive, user } = useContext(AuthContext);
+	const routes = {
+		Home: "/",
+		Ranking: "/ranking",
+		Entrar: "/login",
+		"Cadastrar-se": "/signup",
+		Sair: "/",
+	};
 	const menu = user
 		? ["Home", "Ranking", "Sair"]
 		: ["Entrar", "Cadastrar-se"];
-    const welcomeMessage = `Seja bem-vindo(a), ${user}!`
+
+	const navigate = useNavigate();
+
+	const welcomeMessage = `Seja bem-vindo(a), ${user}!`;
 	return (
 		<HeaderContainer>
 			<SubHeader>
-                <div>
-                    {user && <p>{welcomeMessage}</p>}
-                </div>
+				<div>{user && <p>{welcomeMessage}</p>}</div>
 				<Menu>
 					{menu.map((item, i) => {
 						return (
 							<li
 								key={i}
 								className={setClass(item, active)}
-								onClick={() => setActive(item)}
+								onClick={() => {
+									setActive(item);
+									navigate(routes[item]);
+								}}
 							>
 								{item}
 							</li>
