c95b9900d224f8919709ba0beb33c66880a12da1
encarnacao <cencarnacaoq@gmail.com>
2023-03-02 13:52:01 -0300

    feat: use sticky state hook

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,19 +1,36 @@
+import axios from "axios";
 import { createContext, useState } from "react";
+import useStickyState from "../hooks/sticky";
 
 const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
 	const [user, setUser] = useState(null);
-	const [token, setToken] = useState(null);
-	const [active, setActive] = useState('Entrar');
+	const [token, setToken] = useStickyState("", "token");
+	const [active, setActive] = useState("Entrar");
 	const config = {
 		headers: {
 			Authorization: `Bearer ${token}`,
 		},
 	};
+	async function getUser() {
+		const result = await axios.get("/users/me", config);
+		const username = result.data.name;
+		setUser(username);
+	}
 
 	return (
-		<AuthContext.Provider value={{ user, config, active, setUser, setToken, setActive }}>
+		<AuthContext.Provider
+			value={{
+				user,
+				config,
+				active,
+				setToken,
+				setUser,
+				setActive,
+				getUser,
+			}}
+		>
 			{children}
 		</AuthContext.Provider>
 	);
